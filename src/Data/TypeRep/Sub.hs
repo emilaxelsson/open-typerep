ffcb356b4efec0b3659971e22b345ae45bda9c93
Emil Axelsson <emax@chalmers.se>
2015-04-09 11:17:52 +0200

    Add OVERLAPPING pragma

--- src/Data/TypeRep/Sub.hs
+++ src/Data/TypeRep/Sub.hs
@@ -1,9 +1,21 @@
-{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE UndecidableInstances #-}
+
+#ifndef MIN_VERSION_GLASGOW_HASKELL
+#define MIN_VERSION_GLASGOW_HASKELL(a,b,c,d) 0
+#endif
+  -- MIN_VERSION_GLASGOW_HASKELL was introduced in GHC 7.10
+
+#if MIN_VERSION_GLASGOW_HASKELL(7,10,0,0)
+#else
+{-# LANGUAGE OverlappingInstances #-}
+#endif
 
 -- | This module is only to limit the scope of the @OverlappingInstances@ flag
 
 module Data.TypeRep.Sub where
+
+-- TODO Merge this module with `Data.TypeRep.Internal` when support for < 7.10 is dropped
 
 
 
@@ -23,11 +35,12 @@ class SubUniverse sub sup
     -- | Cast a type representation to a larger universe
     weakenUniverse :: TypeRep sub a -> TypeRep sup a
 
-instance SubUniverse t t
+
+instance {-# OVERLAPPING #-} SubUniverse t t
   where
     weakenUniverse = id
 
-instance (SubUniverse sub sup', sup ~ (t :+: sup')) => SubUniverse sub sup
+instance {-# OVERLAPPING #-} (SubUniverse sub sup', sup ~ (t :+: sup')) => SubUniverse sub sup
   where
     weakenUniverse = sugar . mapAST InjR . desugar . weakenUniverse
 
